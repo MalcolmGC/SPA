f7571fec5e593570d661e9b35069bb6ec10aa0b3
Malcolm Currie <MalcolmGC@users.noreply.github.com>
2016-05-18 13:18:09 -0700

    Javascript Object.create() to Emulate Constructor
    // Object.createAsConstuctor.js
    In the article JavaScript: How to Use Object.create() to Emulate Constructor? at http://xahlee.info/js/javascript_object_create_emulating_constructor.html,
    The instance is created with a long descriptor --  an in real life they are even longer. I would prefer a one line instantiation, e.g.,
         var o2 = fcn(4);
    The attached file rewrites the cited article to do just that. The original descriptor is returned from a function that is invoked as the second argument to Object.create(), instead of having the entire multi-line object repeated as the second argument for each instance.
    The approach is easily extended to multiple arguments in the descriptor.
    This approach is applicable to get rid of 'new' and 'this' in such exercises as in codeAcademy Javascript I - Lesson 21 Custom Constuctors.

---
+++ Object.createAsConstuctor.js
@@ -0,0 +1,46 @@
+// Object.createAsConstuctor.js
+// From: http://xahlee.info/js/javascript_object_create_emulating_constructor.html
+console.log('========================================================');
+// // using Object.create to emulate constructor
+var dad = {a: 3};
+// constructor version
+var Fcn = function (x) {
+    'use strict';
+    this.y = x;
+};
+Fcn.prototype = dad;
+var o1 = new Fcn(4);
+console.log('o1 Prototype: ', Object.getPrototypeOf(o1)); // Object {a: 3}
+console.log('o1: ', o1); // Fcn {y: 4} ...
+console.log('o1.y: ', o1.y); // Fcn {y: 4} ...
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(o1, "y")));
+// Object.Create version
+var yDescriptor = function (x) {
+    'use strict';
+    return {y: {
+        value: x,
+        writable: true,
+        enumerable: true,
+        configurable: true
+    }};
+};
+var fcn = function (x) {
+    'use strict';
+    return Object.create(dad,
+            yDescriptor(x));
+};
+
+var o2 = fcn(4);
+console.log('o2 Prototype: ', Object.getPrototypeOf(o2)); // Object {a: 3}? Why not 4?
+console.log('o2 : ', o2); // Object {a: 3}? Why not 4?
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(o2, "y")));
+// --------------------------------------------------
+// test
+console.log(
+    Object.getPrototypeOf(o1) ===
+    Object.getPrototypeOf(o2)
+); // true
+console.log(
+    JSON.stringify(Object.getOwnPropertyDescriptor(o1, "y")) ===
+    JSON.stringify(Object.getOwnPropertyDescriptor(o2, "y"))
+); // true
